f8311225e844abe7779f398f4c5bdb1bd658f93f
guiyeIbarra <guiyeibarra@hotmail.com>
2025-01-09 08:35:58 -0300

    uno

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+let btn1 = document.getElementById("btn1");
+let estilos = document.getElementById("estilos"); 
+
+btn1.addEventListener("click", function () {
+    estilos.textContent = "Desde mis comienzos en la música, con solo 14 años, he participado en grupos y proyectos de variados estilos; rock, pop, folclore, tango, fusión, cumbia, cuarteto; es decir, mucha mezcla, con gente diferente, por diversión, por trabajo, y en cada vuelta aprendía algo nuevo. Visita mi canal de YouTube";
+    
+});
